ea0a253f14968b7c59a86667fd1b409416ce0e5f
Andrew Holguin <ajholguin@ucdavis.edu>
2021-12-02 10:35:07 -0800

    outline basic functions

---
+++ cimis/__init__.py
@@ -0,0 +1,3 @@
+from cimis.cimis import (
+    get_stations, get_hourly_data, get_daily_data
+)
---
+++ cimis/cimis.py
@@ -0,0 +1,23 @@
+"""
+Python wrapper for the CIMIS weather station API
+"""
+
+def get_stations(all=False):
+    """Get station info.
+    """
+    pass
+
+def get_hourly_data():
+    """Get hourly weather station data
+    """
+    pass
+
+def get_daily_data():
+    """Get daily weather station data
+    """
+    pass
+
+def query_cimis():
+    """Send the query to the CIMIS API
+    """
+    pass
