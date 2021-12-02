dbf738a6b14c6e7ec35823f7f8565f7acde11b40
Andrew Holguin <ajholguin@ucdavis.edu>
2021-12-02 10:58:55 -0800

    add initial documentation

--- cimis/cimis.py
+++ cimis/cimis.py
@@ -4,20 +4,26 @@ Python wrapper for the CIMIS weather station API
 
 def get_stations(all=False):
     """Get station info.
+
+    Parameters
+    ----------
+    all :
+         (Default value = False)
+
+    Returns
+    -------
+
     """
     pass
 
 def get_hourly_data():
-    """Get hourly weather station data
-    """
+    """Get hourly weather station data"""
     pass
 
 def get_daily_data():
-    """Get daily weather station data
-    """
+    """Get daily weather station data"""
     pass
 
 def query_cimis():
-    """Send the query to the CIMIS API
-    """
+    """Send the query to the CIMIS API"""
     pass
